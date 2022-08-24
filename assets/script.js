2fe73e5bc0d9ffef2faae1418c30749186a5f71b
APowers <andrew.powers6901@yahoo.com>
2022-08-24 19:48:40 -0400

    wrote names of functions to lay groundwork for future code

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+
+
+// function to start the game
+function startQuiz() {
+
+}
+
+// function to move to next question
+function nextQuestion() {
+
+}
+
+// function to select answer
+function answerChoice () {
+
+}
+
+// function to submit score
+function submitAnswers() {
+
+}
+
+// function to record score and name
+function highscore() {
+
+}
+
+// function to count down time, also to demerit time if incorrect answer is chosen
+function timer() {
+
+}
